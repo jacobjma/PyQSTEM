5b28aa1955c3b1d8ea9daad658dc603487a4d321
Jacob Madsen <jamad@fysik.dtu.dk>
2017-03-23 13:40:12 +0100

    fixed compile problem on windows with python 2.7

--- setup.py
+++ setup.py
@@ -8,16 +8,26 @@ except ImportError:
 from pkg_resources import get_build_platform
 from setuptools import find_packages
 from Cython.Build import cythonize
+import distutils
 import numpy as np
 import os
+import sys
 
 include_dirs = [np.get_include(),os.path.join(os.getcwd(), 'fftw'),os.path.join(os.getcwd(), 'source/')]
 
 if get_build_platform() == 'win32':
-    library_dirs = ['fftw/win32']
+    if sys.version_info[0] == 3:
+        library_dirs = ['fftw/win32']
+    elif sys.version_info[0] == 2:
+        library_dirs = ['fftw/win32/dll']
+
     libraries = ['libfftw3-3', 'libfftw3f-3', 'libfftw3l-3']
 elif get_build_platform() == 'win-amd64':
-    library_dirs = ['fftw/win64']
+    if sys.version_info[0] == 3:
+        library_dirs = ['fftw/win64']
+    elif sys.version_info[0] == 2:
+        library_dirs = ['fftw/win64/dll']
+
     libraries = ['libfftw3-3', 'libfftw3f-3', 'libfftw3l-3']
 else:
     library_dirs = []
@@ -37,7 +47,7 @@ setup(name='qstem',
                     library_dirs=library_dirs,
                     libraries=libraries,
                     include_dirs=include_dirs,
-                    extra_compile_args=['-std=c++11'], #,'-D MS_WIN64'],
+                    extra_compile_args=['-std=c++11','-D MS_WIN64'],
                     language='c++')),
 
      )
